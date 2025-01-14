578607152d0f640cc15b6caa13e427f00f48a69c
sinwoo-kim <shinwoo5676@gmail.com>
2025-01-14 19:44:52 +0900

    Style: 코드 정리

--- src/main/java/com/example/outsourcingproject/exception/AuthenticationEntryPoint.java
+++ src/main/java/com/example/outsourcingproject/exception/AuthenticationEntryPoint.java
@@ -17,6 +17,6 @@ public class AuthenticationEntryPoint implements org.springframework.security.we
 		AuthenticationException authException) throws IOException, ServletException {
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		response.setContentType("application/json");
-		response.getWriter().write("{\"error\": \"Unauthorized: " + authException.getMessage() + "\"}");
+		response.getWriter().write("{\"error\": \"인증 실패: 유효하지 않은 인증 정보입니다.\"}");
 	}
 }
--- src/main/java/com/example/outsourcingproject/exception/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/outsourcingproject/exception/JwtAccessDeniedHandler.java
@@ -8,15 +8,18 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 		AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
+		log.info("JwtAccessDeniedHandler start = {}", response);
 		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
-		response.getWriter().write("{\"error\": \"Access denied\", \"message\": \"접근 권한이 없습니다.\"}");
+		response.getWriter().write("{\"error\": \"Access denied\", \"message\": \"Access Denied\"}");
+		log.info("JwtAccessDeniedHandler end = {}", response);
 	}
 }
